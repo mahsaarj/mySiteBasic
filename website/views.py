0f02e91797615aaefb3df438e4857f8f64f32472
mahsaarj <103898788+mahsaarj@users.noreply.github.com>
2023-08-29 05:51:42 -0700

    Added blog & website apps and statics

--- website/views.py
+++ website/views.py
@@ -7,4 +7,4 @@ def index_view(request):
 def about_view(request):
     return render(request, 'website/about.html')
 def contact_view(request):
-    return render(request, 'website/contacts.html')
+    return render(request, 'website/contact.html')
